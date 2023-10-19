4a74af8b5f7f82efd7f7f120259879d6ac8d67e6
Edward Tanguay (windows) <edward@tanguay.info>
2023-10-19 07:18:31 +0200

    skills

--- src/components/Skills.ts
+++ src/components/Skills.ts
@@ -1,10 +1,12 @@
 import { ISkill } from "../interfaces";
 
+const showNumber = 10;
+
 export const Skills = (skills: ISkill[]) => {
 	return /*html*/ `
-	<h2>There are ${skills.length} skills.</h2>
+	<h2>There are ${skills.length} skills, here the first ${showNumber}.</h2>
 	<ul>
-		${skills.map(skill => {
+		${skills.slice(0,showNumber).map(skill => {
 			return /*html*/ `<li>${skill.name}</li>`
 		}).join('')}
 	</ul>
--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,14 @@ import { Header } from './components/Header.js';
 import { Skills } from './components/Skills';
 import skills from './data/skills.json';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = /*html*/ `
+(async () => {
+	const response = await fetch('https://edwardtanguay.vercel.app/share/jobs.json');
+	const jobs = await response.json();
+
+	document.querySelector<HTMLDivElement>('#app')!.innerHTML = /*html*/ `
 	${Header('Parcel Site')}
 	<p>Welcome to this site.</p> 
 	<img src="images/code.png?width=10"/>
 	${Skills(skills)}
 `;
+})();
